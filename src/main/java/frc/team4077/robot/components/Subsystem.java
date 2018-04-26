613632e30bcd8fc95aaba4a116fb8406dae588f8
Hillel Coates <hillel.g.coates@gmail.com>
2018-04-25 18:16:00 -0700

    :sparkles: Add base of Drive subsystem. Added Better Javadoc with more tags.

--- src/main/java/frc/team4077/robot/components/Subsystem.java
+++ src/main/java/frc/team4077/robot/components/Subsystem.java
@@ -1,10 +1,17 @@
 package frc.team4077.robot.components;
 
+/**
+ * This is the base class which all subsystems/components inherit. This requires
+ * that each subsystem can print telemetry data to the SmartDashboard as well as
+ * reset any sensors associated with the subsystem.
+ * 
+ * @author FRC 4077 MASH, Hillel Coates
+ */
 public abstract class Subsystem {
     /**
      * Print telemetry associated with this subsystem to the SmartDashboard.
      */
-    public abstract void outputToSmartDashboard();
+    public abstract void sendTelemetry();
 
     /**
      * Reset and zero all sensors associated with the subsystem.
