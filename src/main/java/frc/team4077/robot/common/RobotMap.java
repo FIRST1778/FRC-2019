ffc10561955e5ed0c755d54528dcb8d8fca5989c
Hillel Coates <hillelcoates@yahoo.com>
2018-04-25 20:27:28 -0700

    :zap: Add RobotMap for hardware constants

---
+++ src/main/java/frc/team4077/robot/common/RobotMap.java
@@ -0,0 +1,14 @@
+package frc.team4077.robot.common;
+
+/**
+ * Holds hardware variables as to which ports hardware is attached to.
+ *
+ * @author FRC 4077 MASH, Hillel Coates
+ */
+public class RobotMap {
+  // Drive
+  public static final int LEFT_MASTER_TALON_ID = 1;
+  public static final int RIGHT_MASTER_TALON_ID = 3;
+  public static final int LEFT_SLAVE_TALON_ID = 2;
+  public static final int RIGHT_SLAVE_TALON_ID = 4;
+}
--- src/main/java/frc/team4077/robot/components/Drive.java
+++ src/main/java/frc/team4077/robot/components/Drive.java
@@ -1,36 +1,46 @@
 package frc.team4077.robot.components;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import frc.team4077.robot.common.RobotMap;
+
 /**
  * This is the robot's drivetrain. This class handles the four TalonSRX motor
  * controllers attached to the ganged left and right motors.
  * <p>
  * The drivetrain consists of four (4) TalonSRX motor controllers, four (4) CIM
  * motors, one (1) solenoid, two (2) shifting pistons, and one (1) NavX IMU.
- * 
+ *
  * @author FRC 4077 MASH, Hillel Coates
  * @see Subsystem.java
  */
 public class Drive extends Subsystem {
-    private static Drive mInstance = new Drive();
-    
-    /**
-     * Returns a static instance of Drive, to be used instead of instantiating
-     * new objects of Drive.
-     */         
-    public static Drive getInstance() {
-        return mInstance;
-    }
+  private static Drive mInstance = new Drive();
 
-    private Drive() {
+  private final TalonSRX mLeftMaster, mRightMaster, mLeftSlave, mRightSlave;
 
-    }
+  /**
+   * Returns a static instance of Drive, to be used instead of instantiating
+   * new objects of Drive.
+   */
+  public static Drive getInstance() {
+    return mInstance;
+  }
 
-    @Override
-    public void sendTelemetry() {
-    }
+  private Drive() {
+    mLeftMaster = new TalonSRX(RobotMap.LEFT_SLAVE_TALON_ID);
+    mRightMaster = new TalonSRX(RobotMap.RIGHT_MASTER_TALON_ID);
+    mLeftSlave = new TalonSRX(RobotMap.LEFT_SLAVE_TALON_ID);
+    mRightSlave = new TalonSRX(RobotMap.RIGHT_SLAVE_TALON_ID);
 
-    @Override
-    public void zeroAndReset() {
+    mLeftSlave.set(ControlMode.Follower, RobotMap.LEFT_MASTER_TALON_ID);
+    mRightSlave.set(ControlMode.Follower, RobotMap.RIGHT_MASTER_TALON_ID);
+  }
 
-    }
+  @Override
+  public void sendTelemetry() {}
+
+  @Override
+  public void zeroAndReset() {}
 }
