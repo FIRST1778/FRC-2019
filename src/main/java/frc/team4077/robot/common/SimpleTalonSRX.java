c6885caa060e86345133ad9109ef8f2605cbffaf
Hillel Coates <hillel.g.coates@gmail.com>
2018-05-17 01:09:38 -0700

    :art: Simplify changing control modes

--- src/main/java/frc/team4077/robot/common/SimpleTalonSRX.java
+++ src/main/java/frc/team4077/robot/common/SimpleTalonSRX.java
@@ -20,18 +20,32 @@ public class SimpleTalonSRX extends TalonSRX {
     super(deviceNumber);
   }
 
-  public void setControlMode(ControlMode controlMode) {
-    if (controlMode != ControlMode.Follower) {
-      super.set(controlMode, Double.NaN);
-    }
+  /**
+   * Sets the control mode of the TalonSRX.
+   *
+   * @param controlMode The control mode to set the TalonSRX to. If set as Follower, the second
+   *     parameter will be used.
+   * @param masterId The ID of the master to follow. If controlMode is not Follower, it is not used.
+   */
+  public void setControlMode(ControlMode controlMode, double masterId) {
+    super.set(controlMode, controlMode == ControlMode.Follower ? masterId : Double.NaN);
   }
 
-  public void setControlMode(ControlMode controlMode, int masterId) {
-    if (controlMode == ControlMode.Follower) {
-      super.set(controlMode, masterId);
-    }
-  }
-
+  /**
+   * Sets the output of the TalonSRX, which can mean different things depending on the mode it is
+   * in.
+   *
+   * @param value The output value to send to the TalonSRX. This changes depending on what mode it
+   *     is set to.
+   *     <ul>
+   *       <li>In PercentOutput, the output is between -1.0 and 1.0, with 0.0 as stopped.
+   *       <li>In Current mode, output value is in amperes.
+   *       <li>In Velocity mode, output value is in position change / 100ms.
+   *       <li>In Position mode, output value is in encoder ticks or an analog value, depending on
+   *           the sensor.
+   *       <li>See In Follower mode, the output value is the integer device ID of the talon to
+   *           duplicate.
+   */
   public void set(double value) {
     if (value != lastSet || getControlMode() != lastControlMode) {
       lastSet = value;
