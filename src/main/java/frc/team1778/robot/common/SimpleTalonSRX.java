41bb87f44c7a14d27831b3fd78a3c37ee078e19b
Hillel Coates <hillelcoates@yahoo.com>
2018-05-29 00:24:38 -0700

    :construction: Start swerve drivetrain code

--- src/main/java/frc/team1778/robot/common/SimpleTalonSRX.java
+++ src/main/java/frc/team1778/robot/common/SimpleTalonSRX.java
@@ -12,44 +12,16 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
  */
 public class SimpleTalonSRX extends TalonSRX {
   protected double lastSet = Double.NaN;
-  protected ControlMode lastControlMode = null;
 
   public SimpleTalonSRX(int deviceNumber) {
     super(deviceNumber);
   }
 
-  /**
-   * Sets the control mode of the TalonSRX.
-   *
-   * @param controlMode The control mode to set the TalonSRX to. If set as Follower, the second
-   *     parameter will be used.
-   * @param masterId The ID of the master to follow. If controlMode is not Follower, it is not used.
-   */
-  public void setControlMode(ControlMode controlMode, double masterId) {
-    super.set(controlMode, controlMode == ControlMode.Follower ? masterId : Double.NaN);
-  }
-
-  /**
-   * Sets the output of the TalonSRX, which can mean different things depending on the mode it is
-   * in.
-   *
-   * @param value The output value to send to the TalonSRX. This changes depending on what mode it
-   *     is set to.
-   *     <ul>
-   *       <li>In PercentOutput, the output is between -1.0 and 1.0, with 0.0 as stopped.
-   *       <li>In Current mode, output value is in amperes.
-   *       <li>In Velocity mode, output value is in position change / 100ms.
-   *       <li>In Position mode, output value is in encoder ticks or an analog value, depending on
-   *           the sensor.
-   *       <li>See In Follower mode, the output value is the integer device ID of the talon to
-   *           duplicate.
-   *     </ul>
-   */
-  public void set(double value) {
-    if (value != lastSet || getControlMode() != lastControlMode) {
+  @Override
+  public void set(ControlMode controlMode, double value) {
+    if (value != lastSet) {
       lastSet = value;
-      lastControlMode = getControlMode();
-      super.set(getControlMode(), value);
+      super.set(controlMode, value);
     }
   }
 }
