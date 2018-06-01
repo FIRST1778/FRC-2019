a85a7436ae83a7e235eae9d1de4f7aa3195431a9
Hillel Coates <hillel.g.coates@gmail.com>
2018-05-31 22:19:38 -0700

    :ok_hand: Split Drive class into SwerveModule as handler class

--- src/main/java/frc/team1778/robot/Constants.java
+++ src/main/java/frc/team1778/robot/Constants.java
@@ -6,5 +6,5 @@ package frc.team1778.robot;
  * @author FRC 1778 Chill Out, Hillel Coates
  */
 public class Constants {
-  public static final double DRIVE_MAX_SPEED = 5000.0;
+  public static final double SWERVE_DRIVE_MAX_SPEED = 5500.0;
 }
