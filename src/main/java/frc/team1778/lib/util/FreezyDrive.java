ad7aced765a362a52379ed07495a5d3508a951d6
Hillel Coates <hillel.g.coates@gmail.com>
2018-06-09 22:08:59 -0700

    :sparkles: Add DriveSignal for FreezyDrive

---
+++ src/main/java/frc/team1778/lib/util/FreezyDrive.java
@@ -0,0 +1,16 @@
+package frc.team1778.lib.util;
+
+/**
+ * Implements something similar to "Cheesy Drive", courtesy of 254, and "Culver Drive", courtesy of
+ * 33, making it "Freezy Drive".
+ *
+ * @author FRC 1778 Chill Out
+ */
+public class FreezyDrive {
+  private static final double THROOTLE_DEADBAND = 0.02;
+  private static final double WHEEL_DEADBAND = 0.02;
+
+  private static final double HIGH_GEAR_WHEEL_NON_LINEARITY = 0.65;
+  private static final double LOW_GEAR_WHEEL_NON_LINEARITY = 0.65;
+
+}
