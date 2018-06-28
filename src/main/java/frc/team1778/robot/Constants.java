df5df41573fa2935fdb7f267bb5e249b02e9d8c3
Hillel Coates <hillelcoates@yahoo.com>
2018-06-27 23:17:35 -0700

    :construction: Continue work on Pathfinder with the drivetrain

--- src/main/java/frc/team1778/robot/Constants.java
+++ src/main/java/frc/team1778/robot/Constants.java
@@ -6,14 +6,96 @@ package frc.team1778.robot;
  * @author FRC 1778 Chill Out
  */
 public class Constants {
-  // Pathfinder
-  public static final double TIME_STEP = 0.02;
-  public static final double MAX_VELOCITY = 0;
-  public static final double MAX_ACCELLERATION = 0;
-  public static final double MAX_JERK = 0;
+  public static class Path {
+    public static final PIDConstants PRIMARY_PID = new PIDConstants(2, 0.0, 0.0);
+    public static final PIDConstants GYRO_PID = new PIDConstants(0.0, 0.0, 0.0);
 
-  // Drive
-  public static final double WHEEL_DIAMETER_INCHES = 4.0;
-  public static final double TRACK_WIDTH_INCHES = 0;
-  public static final int ENCODER_TICKS_PER_ROTATION = 10800;
+    public static final double MAX_VELOCITY = 0.25;
+    public static final double KV = 1.0 / MAX_VELOCITY;
+    public static final double MAX_ACCELERATION = 1.0;
+    public static final double KA = 0.05;
+    public static final double MAX_JERK = 30.0;
+    public static final double TRACK_WIDTH = 0.5715;
+    public static final double DELTA_TIME = 0.05;
+
+    public static double ANGLE_OFFSET;
+  }
+
+  public static class Drive {
+    public static final double WHEEL_DIAMETER = 0.1016;
+    public static final int ENCODER_TICKS_PER_ROTATION = 10800;
+  }
+
+  public static class PIDConstants {
+    private double kP;
+    private double kI;
+    private double kD;
+
+    /**
+     * Holds the three PID gain constants requred for a PID loop.
+     *
+     * @param kP the kP to set
+     * @param kI the kI to set
+     * @param kD the kD to set
+     */
+    public PIDConstants(double kP, double kI, double kD) {
+      this.kP = kP;
+      this.kI = kI;
+      this.kD = kD;
+    }
+
+    /**
+     * Sets the new kP.
+     *
+     * @param kP the kP to set
+     */
+    public void setkP(double kP) {
+      this.kP = kP;
+    }
+
+    /**
+     * Sets the new kI.
+     *
+     * @param kI the kI to set
+     */
+    public void setkI(double kI) {
+      this.kI = kI;
+    }
+
+    /**
+     * Sets the new kD.
+     *
+     * @param kD the kD to set
+     */
+    public void setkD(double kD) {
+      this.kD = kD;
+    }
+
+    /**
+     * Returns the current kP gain
+     *
+     * @return the kP gain
+     */
+    public double getkP() {
+      return kP;
+    }
+
+    /**
+     * Returns the current kI gain
+     *
+     * @return the kI gain
+     */
+    public double getkI() {
+      return kI;
+    }
+
+    /**
+     * Returns the current kD gain
+     *
+     * @return the kD gain
+     */
+    public double getkD() {
+      return kD;
+    }
+  }
 }
---
+++ src/main/java/frc/team1778/robot/autonomous/AutoPaths.java
@@ -0,0 +1,11 @@
+package frc.team1778.robot.autonomous;
+
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Waypoint;
+
+public class AutoPaths {
+  public static Waypoint[] testPath =
+      new Waypoint[] {
+        new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0), new Waypoint(0, 0, 0)
+      };
+}
