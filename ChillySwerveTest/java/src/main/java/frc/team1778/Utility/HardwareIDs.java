7035316ab437dbe1899c033de285fdbd8b5334a5
rtrahms <rtrahms@gmail.com>
2018-07-02 17:27:05 -0700

    First GradleRIO vscode commits

---
+++ ChillySwerveTest/java/src/main/java/frc/team1778/Utility/HardwareIDs.java
@@ -0,0 +1,22 @@
+package frc.team1778.Utility;
+
+public class HardwareIDs {
+	
+	// motor controller IDs
+	public static final int FRONT_LEFT_DRIVE_TALON_ID = 3;
+	public static final int FRONT_RIGHT_DRIVE_TALON_ID = 4;
+	public static final int BACK_LEFT_DRIVE_TALON_ID = 5;
+	public static final int BACK_RIGHT_DRIVE_TALON_ID = 6;
+	
+	public static final int FRONT_LEFT_ROTATE_TALON_ID = 7;
+	public static final int FRONT_RIGHT_ROTATE_TALON_ID = 8;
+	public static final int BACK_LEFT_ROTATE_TALON_ID = 9;
+	public static final int BACK_RIGHT_ROTATE_TALON_ID = 10;
+
+	// swerve controller joystick IDs
+	public static final int CONTROL_GAMEPAD_ID = 0;
+	public static final int LEFT_X_AXIS = 0;
+	public static final int LEFT_Y_AXIS = 1;
+	public static final int RIGHT_X_AXIS = 4;
+	public static final int RIGHT_Y_AXIS = 5;
+}
