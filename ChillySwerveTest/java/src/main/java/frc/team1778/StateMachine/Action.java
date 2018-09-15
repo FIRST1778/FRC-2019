57e37916def533cc615dd60a872184098830f2ba
Hillel Coates <hillel.g.coates@gmail.com>
2018-09-15 12:59:27 -0700

    :art: :bug: Fixed the structure of GradleTestProject

--- ChillySwerveTest/java/src/main/java/frc/team1778/StateMachine/Action.java
+++ ChillySwerveTest/java/src/main/java/frc/team1778/StateMachine/Action.java
@@ -4,30 +4,24 @@ import java.util.prefs.Preferences;
 
 public class Action {
 
-	public String name;
-	
-	public Action() {
-		name = "<Generic Action>";
-	}
-	
-	public void initialize() {
-		
-	}
-	
-	public void process() {
-		
-	}
-	
-	public void cleanup() {
-		
-	}
-	
-	public void persistWrite(int counter, Preferences prefs) {
+  public String name;
 
-		// create node for action
-		Preferences actionPrefs = prefs.node(counter + "_" + this.name);
-	
-		// store action class
-		actionPrefs.put("class",this.getClass().toString());
-	}
+  public Action() {
+    name = "<Generic Action>";
+  }
+
+  public void initialize() {}
+
+  public void process() {}
+
+  public void cleanup() {}
+
+  public void persistWrite(int counter, Preferences prefs) {
+
+    // create node for action
+    Preferences actionPrefs = prefs.node(counter + "_" + this.name);
+
+    // store action class
+    actionPrefs.put("class", this.getClass().toString());
+  }
 }
