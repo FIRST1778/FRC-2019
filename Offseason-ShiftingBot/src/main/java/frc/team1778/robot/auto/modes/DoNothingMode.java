bba8ccc3d3171b3e1118877b95d1e6a4ef083dfc
Hillel Coates <hillel.g.coates@gmail.com>
2018-10-15 15:46:30 -0700

    :sparkles: Add TestAutoMode

--- Offseason-ShiftingBot/src/main/java/frc/team1778/robot/auto/modes/DoNothingMode.java
+++ Offseason-ShiftingBot/src/main/java/frc/team1778/robot/auto/modes/DoNothingMode.java
@@ -2,23 +2,10 @@ package frc.team1778.robot.auto.modes;
 
 import frc.team1778.robot.auto.AutoModeBase;
 import frc.team1778.robot.auto.AutoModeEndedException;
-import frc.team1778.robot.auto.actions.PrintToNetworkTableAction;
-import frc.team1778.robot.auto.actions.SeriesAction;
-import frc.team1778.robot.auto.actions.WaitAction;
-import frc.team1778.robot.components.Drive;
-import java.util.Arrays;
 
 public class DoNothingMode extends AutoModeBase {
   @Override
   protected void routine() throws AutoModeEndedException {
-    runAction(
-        new SeriesAction(
-            Arrays.asList(
-                new PrintToNetworkTableAction(
-                    Drive.getinstance().debugTable, "Auto", "Doing Nothing"),
-                new WaitAction(4),
-                new PrintToNetworkTableAction(
-                    Drive.getinstance().debugTable, "Auto", "Done Waiting"))));
-    stop();
+    System.out.println("Do Nothing Mode");
   }
 }
