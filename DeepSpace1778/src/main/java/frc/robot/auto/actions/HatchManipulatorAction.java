cb9f53241a2d025c5d17b1771f3e85828e53fe12
Hillel Coates <hillel.g.coates@gmail.com>
2019-02-24 11:47:50 -0800

    :zap: Rename HatchAction to HatchManipulatorAction and make variables more understandable

---
+++ DeepSpace1778/src/main/java/frc/robot/auto/actions/HatchManipulatorAction.java
@@ -0,0 +1,29 @@
+package frc.robot.auto.actions;
+
+import frc.robot.components.Manipulator;
+
+public class HatchManipulatorAction implements Action {
+
+  private Manipulator manipulator = Manipulator.getInstance();
+  private boolean open;
+
+  public HatchManipulatorAction(boolean grab) {
+    open = grab;
+  }
+
+  @Override
+  public boolean isFinished() {
+    return manipulator.openHatchCollector(open);
+  }
+
+  @Override
+  public void update() {}
+
+  @Override
+  public void done() {}
+
+  @Override
+  public void start() {
+    manipulator.openHatchCollector(open);
+  }
+}
